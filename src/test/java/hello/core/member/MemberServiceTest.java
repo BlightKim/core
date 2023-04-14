a2c4c60b48f93ac14191b7586cdfe5c21d0b2de1
ksebin <ksebin96@gmail.com>
2023-04-14 18:09:16 +0900

    2023-04-14 18:09

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,17 @@
+package hello.core.member;
+
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+    private MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    public void join() {
+
+    }
+
+    @Test
+    public void findMember() {
+
+    }
+}
