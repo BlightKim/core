7680a0444e538068aaee15a708292c25b0992fc0
ksebin <ksebin96@gmail.com>
2023-04-15 10:56:02 +0900

    2023-04-15 10:55

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,5 +1,27 @@
 package hello.core.order;
 
+import hello.core.member.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+    MemberRepository memberRepository = new MemoryMemberRepository();
+    @Test
+    void createOrderTest() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "nameA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(memberId);
+
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        Assertions.assertThat(order.getDisCountPrice()).isEqualTo(1000);
+        Assertions.assertThat(order.calculatePrice()).isEqualTo(9000);
+    }
 
 }
