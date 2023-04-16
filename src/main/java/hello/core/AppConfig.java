cb15bacff99816b7bc349e6eb1369cf2ebbae445
ksebin <ksebin96@gmail.com>
2023-04-16 12:20:29 +0900

    2023-04-16 12:20

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,11 +11,20 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
   public MemberService memberService() {
-    return new MemberServiceImpl(new MemoryMemberRepository());
+
+    return new MemberServiceImpl(memberRepository());
+  }
+
+  private static MemoryMemberRepository memberRepository() {
+    return new MemoryMemberRepository();
   }
 
   public OrderService orderService() {
-    return new OrderServiceImpl(new FixDiscountPolicy(), new MemoryMemberRepository());
+    return new OrderServiceImpl(discountPolicy(), memberRepository());
   }
 
+  public DiscountPolicy discountPolicy() {
+//    return new FixDiscountPolicy();
+    return new RateDiscountPolicy();
+  }
 }
