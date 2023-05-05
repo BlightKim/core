2cebad07950f7cc2f376d8807a11dc60536ab9f4
ksb96 <ksebin96@gmail.com>
2023-05-05 10:48:30 +0900

    perf(BRANCH): Lenovo
    Commit

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -13,16 +13,19 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger = myLoggerProvider.getObject();
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURL().toString();
-        myLogger.setRequestURL(requestURL);
 
+        System.out.println("myLogger = " + myLogger.getClass());
+        myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
+
+        Thread.sleep(1000);
+
         logDemoService.logic("testId");
         return "OK";
     }
